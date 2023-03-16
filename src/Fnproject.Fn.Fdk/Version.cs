d5b01cbd56a3cb7a97d328d8a18f107c8c9e50d2
CI <ci@fnproject.com>
2023-03-16 07:41:54 +0000

    FDK Dotnet: 1.0.12 version release

--- src/Fnproject.Fn.Fdk/Version.cs
+++ src/Fnproject.Fn.Fdk/Version.cs
@@ -2,7 +2,7 @@ namespace Fnproject.Fn.Fdk
 {
     sealed class Version
     {
-        private static readonly string version = "1.0.11";
+        private static readonly string version = "1.0.12";
 
         private Version() { }
 
