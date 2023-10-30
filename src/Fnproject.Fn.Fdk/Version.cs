605b73544c64b8ad857bb1b0098b8df5bc9f7d43
CI <ci@fnproject.com>
2023-10-30 12:01:36 +0000

    Releasing version refs/heads/master

--- src/Fnproject.Fn.Fdk/Version.cs
+++ src/Fnproject.Fn.Fdk/Version.cs
@@ -2,7 +2,7 @@ namespace Fnproject.Fn.Fdk
 {
     sealed class Version
     {
-        private static readonly string version = "1.0.19";
+        private static readonly string version = "1.0.20";
 
         private Version() { }
 
