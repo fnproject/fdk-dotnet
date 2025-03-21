2166abcb921e251d7293b6978fb27f8c6daf7c8f
Alex Shand <alex.shand@oracle.com>
2025-03-21 15:34:24 +0000

    Don't carry bitbucket tags through to github

---
+++ internal/release/github_pubish.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+set -eu
+
+GITHUB_REMOTE=git@github.com:fnproject/fdk-dotnet.git
+BITBUCKET_REMOTE=ssh://git@bitbucket.oci.oraclecorp.com:7999/faas/fdk-dotnet.git
+PUBLISH_BRANCH=github
+TARGET_BRANCH=master
+GIT_USER_EMAIL=ci@fnproject.com
+GIT_USER_NAME=CI
+SSH_KEY_SECRET=ocid1.vaultsecret.oc1.phx.amaaaaaaepf6idiazsfpiv3x3sqmo7xkxn3zci3ntcekqdkstuhnteqoisea
+
+TEMPDIR=$(mktemp -d)
+cd ${TEMPDIR}
+function cleanup {
+    rm -rf ${TEMPDIR}
+}
+trap cleanup EXIT
+
+KEY=$(oci secrets secret-bundle get --auth security_token --region us-phoenix-1 --secret-id ${SSH_KEY_SECRET} --raw-output --query 'data."secret-bundle-content".content' | base64 -D)
+echo "$KEY" >key
+chmod 600 key
+
+export GIT_SSH_COMMAND="ssh -i ${TEMPDIR}/key"
+git clone ${GITHUB_REMOTE} github
+git clone ${BITBUCKET_REMOTE} bitbucket
+
+pushd bitbucket
+git fetch
+git checkout ${PUBLISH_BRANCH}
+git reset --hard origin/${PUBLISH_BRANCH}
+popd
+
+pushd github
+git fetch
+git checkout ${TARGET_BRANCH}
+git reset --hard origin/${TARGET_BRANCH}
+git remote add bitbucket ../bitbucket
+git fetch bitbucket
+git config user.email ${GIT_USER_EMAIL}
+git config user.name ${GIT_USER_NAME}
+git merge --ff bitbucket/${PUBLISH_BRANCH}
+git push
+
+VERSION_FILE=src/Fnproject.Fn.Fdk/Version.cs
+RELEASE_VERSION=$(grep -m1 -Eo "[0-9]+\.[0-9]+\.[0-9]+" ${VERSION_FILE})
+echo "Release version: ${RELEASE_VERSION}"
+git tag -f -a ${RELEASE_VERSION} -m "version: ${RELEASE_VERSION}"
+git push origin ${RELEASE_VERSION}
