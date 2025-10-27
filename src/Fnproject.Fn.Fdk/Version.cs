066c0c798106a519e251ce639773ef439b9286b0
CI <ci@fnproject.com>
2025-10-27 06:22:02 +0000

    Releasing version 1.0.49

--- src/Fnproject.Fn.Fdk/Version.cs
+++ src/Fnproject.Fn.Fdk/Version.cs
@@ -2,7 +2,7 @@ namespace Fnproject.Fn.Fdk
 {
     sealed class Version
     {
-        private static readonly string version = "1.0.48";
+        private static readonly string version = "1.0.49";
 
         private Version() { }
 
